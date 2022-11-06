0b93746d2dcd0852cbe4a512dfa6c6732f88f96c
Huy <lenambaohuy107@gmail.com>
2022-11-06 15:42:50 +0700

    practice updated

--- index.js
+++ index.js
@@ -57,14 +57,64 @@ function handleCal() {
     total = 50 * 500 + 50 * 650 + (amount - 100) * 850;
   } else if (amount <= 350) {
     total = 50 * 500 + 50 * 650 + 100 * 850 + (amount - 200) * 1100;
-  } else if (amount > 350){
-    total = 50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + (amount - 350) * 1300;
+  } else if (amount > 350) {
+    total =
+      50 * 500 + 50 * 650 + 100 * 850 + 150 * 1100 + (amount - 350) * 1300;
   }
 
-  var totalPrice = "Tổng tiền là:" + total
+  var totalPrice = "Tổng tiền là:" + total;
   document.getElementById("totalPrice").innerHTML = totalPrice;
 }
 
-
 //3
+function handleCalTax() {
+  var userName = document.getElementById("nameUser").value;
+  var numberOfPeople = document.getElementById("numberOfDependPeople").value;
+  var salary = +document.getElementById("totalSalary").value;
+  var tax = 0;
+  var totalTax = salary - 4 - numberOfPeople * 1.6;
 
+  if (totalTax > 0 && totalTax <= 60) {
+    tax = totalTax * (5 / 100);
+  } else if (totalTax > 60 && totalTax <= 120) {
+    tax = 60 * (5 / 100) + (totalTax - 60) * (10 / 100);
+  } else if (totalTax > 0 && totalTax <= 210) {
+    tax = 60 * (5 / 100) + 60 * (10 / 100) + (totalTax - 120) * (15 / 100);
+  } else if (totalTax > 0 && totalTax <= 384) {
+    tax =
+      60 * (5 / 100) +
+      60 * (10 / 100) +
+      90 * (15 / 100) +
+      (totalTax - 210) * (20 / 100);
+  } else if (totalTax > 0 && totalTax <= 624) {
+    tax =
+      60 * (5 / 100) +
+      60 * (10 / 100) +
+      90 * (15 / 100) +
+      174 * (20 / 100) +
+      (totalTax - 384) * (25 / 100);
+  } else if (totalTax > 0 && totalTax <= 960) {
+    tax =
+      60 * (5 / 100) +
+      60 * (10 / 100) +
+      90 * (15 / 100) +
+      174 * (20 / 100) +
+      240 * (25 / 100) +
+      (totalTax - 624) * (30 / 100);
+  } else if (totalTax > 960) {
+    tax =
+      60 * (5 / 100) +
+      60 * (10 / 100) +
+      90 * (15 / 100) +
+      174 * (20 / 100) +
+      240 * (25 / 100) +
+      336 * (30 / 100) +
+      (totalTax - 960) * (35 / 100);
+  } else {
+    alert("Lo đi làm đi nhé bro !!");
+  }
+
+  var result =
+    "Tổng thuế thu nhập của ông/bà " + userName + " là:" + tax + " triệu";
+  document.getElementById("tax").innerHTML = result;
+}
